69993a00396cb09bee36480e29c9cfad4801fe77
Kai Davenport <kaiyadavenport@gmail.com>
2026-06-03 15:11:43 +0100

    feat(home): clickable Storyverse / Future Proof branch tips

--- apps/web/src/home/Scene.tsx
+++ apps/web/src/home/Scene.tsx
@@ -1,7 +1,9 @@
 import { Canvas } from '@react-three/fiber'
 import { Spine } from './Spine'
 import { Constellation } from './Constellation'
+import { BranchTip } from './BranchTip'
 import { COLORS } from './colors'
+import { GRAPH } from './graph'
 
 export default function Scene() {
   return (
@@ -10,6 +12,8 @@ export default function Scene() {
         <color attach="background" args={[COLORS.black]} />
         <Spine />
         <Constellation />
+        <BranchTip title={GRAPH.tips.storyverse.title} pos={GRAPH.tips.storyverse.pos} route={GRAPH.tips.storyverse.route} up />
+        <BranchTip title={GRAPH.tips.futureProof.title} pos={GRAPH.tips.futureProof.pos} route={GRAPH.tips.futureProof.route} up={false} />
       </Canvas>
     </div>
   )
